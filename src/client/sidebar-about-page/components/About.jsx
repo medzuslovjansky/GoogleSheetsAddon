a471fee0fc9d18aa98f086a1fbff6f051b696cb0
Yaroslav Serhieiev <noomorph@gmail.com>
2021-02-26 11:35:06 +0200

    wip: initial configuration and components

--- src/client/sidebar-about-page/components/About.jsx
+++ src/client/sidebar-about-page/components/About.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Link,
+  TextField,
+} from '@material-ui/core';
+import {
+  SkipPrevious,
+  FastRewind,
+  SkipNext,
+  FastForward,
+} from '@material-ui/icons';
 
 const About = () => (
-  <div>
+  <Box>
+    <Navigation />
     <p>
       <b>☀️ React app inside a sidebar! ☀️</b>
     </p>
@@ -12,15 +27,41 @@ const About = () => (
     <p>
       Visit the Github repo for more information on how to use this project.
     </p>
-    <p>- Elisha Nuchi</p>
-    <a
+    <p>
+      <Button variant="contained" color="primary" onClick={() => alert('Test')}>
+        Elisha Nuchi
+      </Button>
+    </p>
+    <Link
       href="https://www.github.com/enuchi/React-Google-Apps-Script"
       target="_blank"
       rel="noopener noreferrer"
     >
       React + Google Apps Script
-    </a>
-  </div>
+    </Link>
+  </Box>
 );
+
+const Navigation = () => {
+  return (
+    <Container>
+      <ButtonGroup size="small" aria-label="small outlined button group">
+        <Button>
+          <SkipPrevious />
+        </Button>
+        <Button>
+          <FastRewind />
+        </Button>
+        <TextField size="small" label="ID" variant="outlined" />
+        <Button>
+          <FastForward />
+        </Button>
+        <Button>
+          <SkipNext />
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
+};
 
 export default About;
